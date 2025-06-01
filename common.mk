1e14ad4db1aadcba4d0d4c05f32df28568c26dac
zebraoffersp <69884831+zebraoffersp@users.noreply.github.com>
2025-06-01 16:55:10 +0700

    1

---
+++ common.mk
@@ -0,0 +1,50 @@
+#Common headers
+common_includes := $(call project-path-for,qcom-display)/libgralloc
+common_includes += $(call project-path-for,qcom-display)/liboverlay
+common_includes += $(call project-path-for,qcom-display)/libcopybit
+common_includes += $(call project-path-for,qcom-display)/libqdutils
+common_includes += $(call project-path-for,qcom-display)/libhwcomposer
+common_includes += $(call project-path-for,qcom-display)/libexternal
+common_includes += $(call project-path-for,qcom-display)/libqservice
+common_includes += $(call project-path-for,qcom-display)/libvirtual
+
+common_header_export_path := qcom/display
+
+#Common libraries external to display HAL
+common_libs := liblog libutils libcutils libhardware
+
+#Common C flags
+common_flags := -DDEBUG_CALC_FPS -Wno-missing-field-initializers
+common_flags += -Werror -Wno-unused-parameter
+
+
+ifeq ($(ARCH_ARM_HAVE_NEON),true)
+    common_flags += -D__ARM_HAVE_NEON
+endif
+
+ifneq ($(filter msm8974 msm8x74 msm8226 msm8x26,$(TARGET_BOARD_PLATFORM)),)
+    # TODO: This define makes us pick a few inline functions
+    # from the kernel header media/msm_media_info.h. However,
+    # the bionic clean_headers utility scrubs them out.
+    # Figure out a way to import those macros correctly
+    # common_flags += -DVENUS_COLOR_FORMAT
+    common_flags += -DMDSS_TARGET
+endif
+
+ifeq ($(DISPLAY_DEBUG_SWAPINTERVAL),true)
+    common_flags += -DDEBUG_SWAPINTERVAL
+endif
+
+# Executed only on QCOM BSPs
+ifeq ($(TARGET_USES_QCOM_BSP),true)
+# This flag is used to compile out any features that depend on framework changes
+    common_flags += -DQCOM_BSP
+endif
+
+ifeq ($(TARGET_DISPLAY_USE_RETIRE_FENCE),true)
+    common_flags += -DUSE_RETIRE_FENCE
+endif
+
+ifneq ($(TARGET_DISPLAY_INSECURE_MM_HEAP),true)
+    common_flags += -DSECURE_MM_HEAP
+endif
---
+++ libhwcomposer/Android.mk
@@ -0,0 +1,31 @@
+LOCAL_PATH := $(call my-dir)
+include $(LOCAL_PATH)/../common.mk
+include $(CLEAR_VARS)
+
+LOCAL_MODULE                  := hwcomposer.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE_RELATIVE_PATH    := hw
+LOCAL_VENDOR_MODULE           := true
+LOCAL_MODULE_TAGS             := optional
+LOCAL_C_INCLUDES              := $(common_includes) \
+                                 $(TOP)/external/skia/include/core \
+                                 $(TOP)/external/skia/include/images
+LOCAL_SHARED_LIBRARIES        := $(common_libs) libEGL liboverlay \
+                                 libexternal libqdutils libhardware_legacy \
+                                 libdl libmemalloc libqservice libsync \
+                                 libbinder libmedia libvirtual libhwui
+LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdhwcomposer\"
+ifeq ($(GET_DISPLAY_SECURE_STATUS_FROM_HWC),true)
+    LOCAL_CFLAGS += -DGET_DISPLAY_SECURE_STATUS_FROM_HWC
+endif
+LOCAL_HEADER_LIBRARIES        := generated_kernel_headers
+LOCAL_SRC_FILES               := hwc.cpp          \
+                                 hwc_utils.cpp    \
+                                 hwc_uevents.cpp  \
+                                 hwc_vsync.cpp    \
+                                 hwc_fbupdate.cpp \
+                                 hwc_mdpcomp.cpp  \
+                                 hwc_copybit.cpp  \
+                                 hwc_qclient.cpp  \
+                                 hwc_dump_layers.cpp
+
+include $(BUILD_SHARED_LIBRARY)
