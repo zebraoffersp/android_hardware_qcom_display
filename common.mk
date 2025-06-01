7cc22883f3fb50c121c9bb5cf0283ed53a6bbd3e
zebraoffersp <69884831+zebraoffersp@users.noreply.github.com>
2025-06-01 17:02:23 +0700

    1

---
+++ common.mk
@@ -0,0 +1,52 @@
+#Common headers
+common_includes := $(call project-path-for,qcom-display)/libgralloc
+common_includes += $(call project-path-for,qcom-display)/liboverlay
+common_includes += $(call project-path-for,qcom-display)/libcopybit
+common_includes += $(call project-path-for,qcom-display)/libqdutils
+common_includes += $(call project-path-for,qcom-display)/libhwcomposer
+common_includes += $(call project-path-for,qcom-display)/libexternal
+common_includes += $(call project-path-for,qcom-display)/libqservice
+common_includes += $(call project-path-for,qcom-display)/libvirtual
+common_includes += $(call project-path-for,qcom-display)/libhdmi
+
+ifeq ($(TARGET_USES_POST_PROCESSING),true)
+    common_flags     += -DUSES_POST_PROCESSING
+    common_includes  += $(TARGET_OUT_HEADERS)/pp/inc
+endif
+
+common_header_export_path := qcom/display
+
+#Common libraries external to display HAL
+common_libs := liblog libutils libcutils libhardware
+
+#Common C flags
+common_flags := -DDEBUG_CALC_FPS -Wno-missing-field-initializers
+common_flags += -Werror -Wno-error=unused-parameter 
+
+ifeq ($(ARCH_ARM_HAVE_NEON),true)
+    common_flags += -D__ARM_HAVE_NEON
+endif
+
+ifeq ($(call is-board-platform-in-list, msm8974 msm8226 msm8610 apq8084 \
+        mpq8092 msm_bronze msm8916), true)
+    common_flags += -DVENUS_COLOR_FORMAT
+    common_flags += -DMDSS_TARGET
+endif
+
+ifeq ($(call is-board-platform-in-list, mpq8092 msm_bronze msm8916), true)
+    #XXX: Replace with check from MDP when available
+    common_flags += -DVPU_TARGET
+endif
+
+ifeq ($(DISPLAY_DEBUG_SWAPINTERVAL),true)
+    common_flags += -DDEBUG_SWAPINTERVAL
+endif
+
+# Executed only on QCOM BSPs
+ifeq ($(TARGET_USES_QCOM_BSP),true)
+# Enable QCOM Display features
+    common_flags += -DQTI_BSP
+endif
+ifneq ($(call is-platform-sdk-version-at-least,18),true)
+    common_flags += -DANDROID_JELLYBEAN_MR1=1
+endif
